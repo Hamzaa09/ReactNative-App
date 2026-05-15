5151e42ea1852b6b9899647e91b7a7abc45085ea
Hamzaa09 <hamza036513@gmail.com>
2026-05-15 22:37:19 +0500

    Setup Nativewind

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import '../global.css'
+
+export default function Index() {
+  return (
+    <View className="bg-pink-400 h-screen flex justify-center items-center">
+      <Text className="bg-blue-400">Hamza Here...!!</Text>
+    </View>
+  );
+}
