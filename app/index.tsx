12548f3ab65c5e30b690b93f9c34b4a4e55f3ef3
Hamzaa09 <hamza036513@gmail.com>
2026-05-19 00:02:01 +0500

    Implement tab navigation with home, search, saved, and profile screens

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,10 @@
 import { Text, View } from "react-native";
 import '../global.css'
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Redirect } from "expo-router";
 
 export default function Index() {
   return (
-    <View className="bg-pink-400 h-screen flex justify-center items-center">
-      <Text className="bg-blue-400">Hamza Here...!!</Text>
-    </View>
+    <Redirect href="/(root)/(tabs)/home" />
   );
 }
